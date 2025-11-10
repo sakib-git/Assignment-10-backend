092ae927dfefc35a4191c3a1aefb42b00d1d766d
sakib-git <sakibulhasan32002@gmail.com>
2025-11-10 08:30:22 +0600

    set up done

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const cors = require('cors')
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mongodb.0ps5adl.mongodb.net/?appName=MongoDB`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+   
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`)
+})
