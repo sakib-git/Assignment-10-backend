e6223a2225548bf68041ebc118c952ef4feb3b0d
sakib-git <sakibulhasan32002@gmail.com>
2025-11-11 11:36:30 +0600

    category done

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mongodb.0ps5adl.mongodb.net/?appName=MongoDB`;
 
@@ -22,34 +21,45 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
-   
- const db = client.db('assignment-db')
- const billCollection = db.collection('Bills')
- 
- app.get('/bills', async (req, res) => {
-  const reuslt = await billCollection.find().toArray()
-  res.send(reuslt)
- })
 
- app.get('/bills-details/:id', async (req, res) => {
-  const {id}  = req.params
-  const objectId = new ObjectId(id)
-  const reuslt = await billCollection.findOne({_id : objectId})
+    const db = client.db('assignment-db');
+    const billCollection = db.collection('Bills');
+    const categoryCollection = db.collection('category');
 
-  res.send(reuslt)
- })
+    app.get('/bills', async (req, res) => {
+      const category = req.query.category;
+      if (category) {
+        const allBills = await billCollection.find().toArray();
+        const categoryBasedBills = allBills.filter((bill) => bill.category.toLowerCase() === category);
+        res.send(categoryBasedBills);
+        return;
+      }
 
+      const result = await billCollection.find().toArray();
+      res.send(result);
+    });
 
+    app.get('/latest-bills', async (req, res) => {
+      const result = await billCollection.find().sort({ date: -1 }).limit(6).toArray();
+      res.send(result);
+    });
 
+    app.get('/bills-details/:id', async (req, res) => {
+      const { id } = req.params;
+      const objectId = new ObjectId(id);
+      const reuslt = await billCollection.findOne({ _id: objectId });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      res.send(reuslt);
+    });
+
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -57,7 +67,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.listen(port, () => {
-  console.log(`server listening on port ${port}`)
-})
+  console.log(`server listening on port ${port}`);
+});
