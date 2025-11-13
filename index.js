f89c8a94407bd3376658e361c1b77f05965f2a15
sakib-git <sakibulhasan32002@gmail.com>
2025-11-13 15:16:13 +0600

    minor fix

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // await client.connect();
+    await client.connect();
 
     await client.db('admin').command({ ping: 1 });
     console.log('MongoDB connected successfully!');
